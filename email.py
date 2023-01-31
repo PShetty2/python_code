b5abb0910bde51253253604ca00bba141a883fe6
Puneet Shetty <shettypuneet2@gmail.com>
2023-01-31 12:24:35 -0500

    next change

--- email.py
+++ email.py
@@ -2,6 +2,8 @@ email =input("Enter an email message: ")
 index = email.index('@')
 if len(email) >= 10 and len(email) <= 20:
     print("The email is valid.")
+else:
+    print("The email is not valid.")
 
 
 
